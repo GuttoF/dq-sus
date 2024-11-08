c5e2b7acf74d982e8d9bd08e919dca314cca4fc3
“Gutto <guttolaudie@gmail.com>
2024-11-08 15:06:19 -0300

    fix(extraction): :zap: boost performance loading one per one in db

--- src/extract/get_raw.py
+++ src/extract/get_raw.py
@@ -22,7 +22,7 @@ db_path.mkdir(parents=True, exist_ok=True)
 
 def extract_parquet(
     disease: str, years: Union[int, list[int]], file_path: Path = parquet_path
-) -> pd.DataFrame:
+) -> list[Path]:
     valid = ["ZIKA", "CHIK", "DENG"]
     if disease not in valid:
         logging.error(
@@ -35,42 +35,55 @@ def extract_parquet(
     if isinstance(years, int):
         years = [years]
 
-    data_frames = []
+    file_paths = []
     for year in years:
         year_str = str(year)[2:]
         file_name = file_path / f"{disease}BR{year_str}.parquet"
 
-        if file_name.exists():
-            logging.info(f"File located in {file_name} already exists, loading data.")
-            data = pd.read_parquet(file_name)
-        else:
+        if not file_name.exists():
             sinan = SINAN().load()
             file = sinan.get_files(disease, year)
             sinan.download(file, local_dir=str(file_path))
             logging.info(f"File saved in {file_path}")
-            data = pd.read_parquet(file_name)
 
-        data_frames.append(data)
+        file_paths.append(file_name)
 
-    return pd.concat(data_frames, ignore_index=True)
+    return file_paths
 
 
-def extract_to_duck(data: pd.DataFrame, file_path: Path = db_path) -> None:
+def insert_parquet_to_duck(files: list[Path], file_path: Path = db_path) -> None:
+    duck_path = file_path / "db.db"
+    if duck_path.exists():
+        logging.info("Database already exists, it will be overwritten.")
+        os.remove(duck_path)
+
     try:
-        duck_path = file_path / "db.db"
-        if duck_path.exists():
-            logging.info("Database already exists, it will be overwritten.")
-            os.remove(duck_path)
-
         conn = duckdb.connect(str(duck_path))
-        conn.execute("CREATE TABLE IF NOT EXISTS sinan AS SELECT * FROM data")
+
+        for i, file in enumerate(files):
+            logging.info(f"Inserting data from {file} into the database.")
+            data = pd.read_parquet(file)
+
+            conn.register("temp_table", data)
+
+            if i == 0:
+                conn.execute("CREATE TABLE sinan AS SELECT * FROM temp_table")
+            else:
+                conn.execute("INSERT INTO sinan SELECT * FROM temp_table")
+
+            conn.unregister("temp_table")
+            del data
+
         conn.close()
-        logging.info("Database created")
+        logging.info("All data inserted into the database successfully.")
     except Exception as e:
         logging.error(f"Error: {e}")
+        if conn:
+            conn.close()
 
 
 if __name__ == "__main__":
     years = [2022, 2023]
-    data = extract_parquet("CHIK", years)
-    extract_to_duck(data)
+    files = extract_parquet("CHIK", years)
+    insert_parquet_to_duck(files)
+
